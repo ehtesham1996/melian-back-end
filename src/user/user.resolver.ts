606b8ded6acac429b8e0ff4e5fa67a73c7fc7428
ehtesham1996 <ehteasham@gmail.com>
2020-12-31 20:11:30 +0500

    upload mutation added

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -5,10 +5,11 @@ import { CreateUserInput } from './dto/create-user.input';
 import { LoginInput, LoginOutput } from './dto/login-user.input';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
-
+import { SignedUrlResponse } from './types/signed-url-response.type';
+import { S3 } from 'aws-sdk';
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Mutation(() => User)
   async signup(@Args('User') createUserInput: CreateUserInput) {
@@ -26,6 +27,33 @@ export class UserResolver {
     return user;
   }
 
+  @Mutation(() => SignedUrlResponse)
+  async getSignedUrlForUpload(
+    @Args('filename') filename: string,
+    @Args('filetype') filetype: string
+  ): Promise<SignedUrlResponse> {
+
+    const s3 = new S3({
+      signatureVersion: 'v4',
+      region: 'us-east-2',
+    });
+
+    const s3Params = {
+      Bucket: process.env.S3_BUCKET || 'com.serverless.hazree.deploys',
+      Key: filename,
+      Expires: 60,
+      ContentType: filetype,
+      ACL: 'public-read',
+    };
+
+    const signedRequest = await s3.getSignedUrl('putObject', s3Params);
+    const url = `https://${process.env.S3_BUCKET ||  'com.serverless.hazree.deploys' }.s3.amazonaws.com/${filename}`;
+
+    return {
+      signedRequest,
+      url
+    };
+  }
   // @Query(() => [User], { name: 'user' })
   // findAll() {
   //   return this.userService.findAll();
